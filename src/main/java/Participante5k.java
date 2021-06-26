be064651036423327a53b0db49cf35e678ace8ef
MedardoGarcesM <medjegar@espol.edu.ec>
2021-06-26 17:07:35 -0500

    carga de lo avanzado

---
+++ src/main/java/Participante5k.java
@@ -0,0 +1,13 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author Medardo Garces
+ */
+public class Participante5k {
+    private float tiempo;
+}
