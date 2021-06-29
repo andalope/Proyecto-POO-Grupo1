8723672debbbb343922d67ae5daec5e1f9132f35
andalope <andalope@espol.edu.ec>
2021-06-29 06:38:09 -0500

    carrera 5k finalizada y se creo el menu de torneo de videojuegos

--- PROYECTOO/src/main/java/eventos/Carrera5K.java
+++ PROYECTOO/src/main/java/eventos/Carrera5K.java
@@ -27,6 +27,120 @@ public Carrera5K(int id, LocalDate fecha, LocalTime hora, String[] premios) {
 
 public Carrera5K(int id, LocalDate fecha, LocalTime hora, String[] premios, boolean terminado) {
         super(id, fecha, hora, premios, terminado);
+}
+
+//sobreescritura de metododos de la clase competencia
+@Override
+public void presentarMenu() {
+    System.out.println("Estas dentro de la seccion Carreras 5K");
+    System.out.println("Carreras creadas");
+        
+    for (Competencia c: Sistema.getInstance().getListaCompetencia()) {
+        if (c.getClass() == this.getClass()) {
+            System.out.println(c);
+        }
     }
-    
+    System.out.println("");
+        
+    System.out.println("1. Crear nueva carrera");
+    System.out.println("2. Registrar participantes");
+    System.out.println("3. Registrar ganadores");
+    System.out.println("4. Volver al menu principal");
+    System.out.println("");
+    System.out.print("Escoge una opcion: ");
+    }
+
+@Override
+public void registarParticipante() {
+    if (terminado) {
+        System.out.println("Carrera cerrada, no se pueden registrar más estudiantes");
+        System.out.println("");
+    } else {
+        System.out.println("Registro de participantes en carrera del " + getFecha());
+        int id;
+        do {
+            System.out.print("ID del estudiante: ");
+            id = Integer.parseInt(sistema.getScanner().nextLine());
+            Estudiante e = sistema.buscarEstudiante(id);
+            Candidato repetido = buscarParticipante(id);
+            if (e == null || repetido != null) {
+                System.out.println("Id no existe o estudiante ya registrado");
+            } else{
+                listaParticipantes.add(new Participante5K(e));
+                System.out.println("Estudiante " + e.getNombre() + " registrado");
+            }
+        } while (id != 0);
+    }
+}
+
+@Override
+public void registrarGanadores() {
+    if (terminado) {
+        System.out.println("Carrera cerrada, no se pueden modificar los ganadores");
+        System.out.println("");
+    } else if (listaParticipantes.size() >= 3){
+        int i = 0;
+        do {
+            System.out.println("Ingreso del puesto N°" + (i + 1));
+            System.out.print("ID: ");
+            int id = Integer.parseInt(sistema.getScanner().nextLine());
+            Participante5K p = (Participante5K) buscarParticipante(id);
+            if (p == null || ganadorRepetido(id)) {
+                System.out.println("Id no existe o ganador ya incluido");
+
+            } else {
+                ganadores[i] = p;
+                System.out.print("Tiempo: ");
+                double tiempo = Double.parseDouble(sistema.getScanner().nextLine());
+                p.setTiempo(tiempo);
+                i++;
+            }
+        } while (i < 3);
+    } else {
+        System.out.println("No hay suficientes participantes registrados");
+        System.out.println("");
+    }
+    terminado = true;
+}
+
+@Override
+public void opcionUno() {
+    System.out.println("No debe implementarse");
+    System.out.println("");
+}
+
+@Override
+public void opcionDos() {
+    System.out.print("Ingrese ID de la carrera: ");
+}
+
+@Override
+public void opcionTres() {
+    System.out.print("Ingrese ID de la carrera: ");
+}
+
+@Override
+public Candidato buscarParticipante(int id) {
+    for (Candidato c : listaParticipantes) {
+        if (c.getId() == id) {
+            return c;
+        }
+    }
+    return null;
+}
+
+@Override
+public String toString() {
+    return getId() + " " + getFecha() + " " + getHora() + " " + listaParticipantes.size();
+}
+
+@Override
+public boolean ganadorRepetido(int id) {
+    for (int i = 0; i < ganadores.length; i++) {
+        if (ganadores[i] != null && ganadores[i].getId() == id) {
+            return true;
+        }
+    }
+    return false;
+}
 }
--- PROYECTOO/src/main/java/eventos/TorneoVideojuego.java
+++ PROYECTOO/src/main/java/eventos/TorneoVideojuego.java
@@ -5,10 +5,64 @@
  */
 package eventos;
 
+import java.time.LocalDate;
+import java.time.LocalTime;
+import participantes.Candidato;
+
 /**
  *
  * @author danil
  */
-public class TorneoVideojuego {
-    private String nombreVideojuego;   
+public class TorneoVideojuego extends Competencia{
+private String nombreVideojuego;
+
+public TorneoVideojuego() {
+}
+
+public TorneoVideojuego(int id, LocalDate fecha, LocalTime hora, String[] premios, String nombreVideojuego) {
+    super(id, fecha, hora, premios);
+    this.nombreVideojuego = nombreVideojuego;
+}
+
+@Override
+public void registarParticipante() {
+    throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+}
+
+@Override
+public void registrarGanadores() {
+    throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+}
+
+@Override
+public void presentarMenu() {
+    throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+}
+
+@Override
+public void opcionUno() {
+    throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+}
+
+@Override
+public void opcionDos() {
+    throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+}
+
+@Override
+public void opcionTres() {
+    throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+}
+
+@Override
+public Candidato buscarParticipante(int id) {
+    throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+}
+
+@Override
+public boolean ganadorRepetido(int id) {
+    throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+}
+
+
 }
