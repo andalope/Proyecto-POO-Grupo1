27fb0518d0a32f2a3d80cc30ebc26e6fb65d07af
MedardoGarcesM <medjegar@espol.edu.ec>
2021-07-04 08:22:16 -0500

    añadidos los metodos a videjoueg

--- Proyecto/src/eventos/TorneoVideojuego.java
+++ Proyecto/src/eventos/TorneoVideojuego.java
@@ -17,15 +17,70 @@ public class TorneoVideojuego extends Competencia{
         super(id, fecha, hora, premios);
         this.nombreVideojuego = nombreVideojuego;
     }
-
+    public TorneoVideojuego(String id, LocalDate fecha, LocalTime hora, String[] premios, String nombreVideojuego, boolean terminado) {
+        super(id, fecha, hora, premios, terminado);
+        this.nombreVideojuego = nombreVideojuego;
+    }
+// Metodo que registra los participantes en el torneo
     @Override
     public void registarParticipante() {
-        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+        if (terminado) {
+            System.out.println("Torneo cerrada, no se pueden registrar más estudiantes");
+            System.out.println("");
+        } else {
+            System.out.println("Registro de participantes en torneo " + nombreVideojuego);
+            String id;
+            do {
+                System.out.print("ID del estudiante: ");
+                id = sistema.getScanner().nextLine();
+
+                Estudiante e = sistema.buscarEstudiante(id);
+                Candidato repetido = buscarParticipante(id);
+
+                if (id.equals("0")) {
+                    System.out.println("Volviendo al menu principal");
+                } else if (e == null || repetido != null) {
+                    System.out.println("Id no existe o estudiante ya registrado");
+                } else {
+                    addParticipante(e);
+                    System.out.println("Estudiante " + e.getNombre() + " registrado");
+                }
+            } while (!id.equals("0"));
+        }
     }
+    // metodo que registra los ganadores en eltorneo
+    
 
     @Override
     public void registrarGanadores() {
-        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+       if (terminado) {
+            System.out.println("Torneo cerrado, no se pueden modificar los ganadores");
+            System.out.println("");
+        } else if (listaParticipantes.size() >= 3) {
+            int i = 0;
+            do {
+                System.out.println("Ingreso del puesto N°" + (i + 1));
+                System.out.print("ID: ");
+                String id = sistema.getScanner().nextLine();
+
+                Estudiante e = (Estudiante) buscarParticipante(id);
+
+                if (e == null || ganadorRepetido(id)) {
+                    System.out.println("Id no existe o ganador ya incluido");
+                    System.out.println("");
+                } else {
+                    System.out.println("Estudiante " + e + " ganador");
+                    System.out.println("");
+                    ganadores[i] = e;
+                    i++;
+                }
+            } while (i < 3);
+            terminado = true;
+        } else {
+            System.out.println("No hay suficientes participantes registrados");
+            System.out.println("");
+        }
+     
     }
 
     @Override
